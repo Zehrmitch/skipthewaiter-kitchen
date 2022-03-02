d6f004fa47e25e63c4bce1aa0cee6f13566b343a
kylekapetan <60860783+kylekapetan@users.noreply.github.com>
2022-03-02 18:35:53 -0500

    MenuForm
    MenuForm has saving and delete functionality

--- src/Pages/MenuBuilder/MenuForm.js
+++ src/Pages/MenuBuilder/MenuForm.js
@@ -1,46 +1,55 @@
 import React, {useState} from 'react';
 import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 import JSONPretty from 'react-json-pretty';
-import { PlusIcon, XIcon } from '@heroicons/react/solid';
+import { PlusIcon, MinusIcon } from '@heroicons/react/solid';
 
 
 
 
 const MenuForm = () => {
 
-    const [inputFields, setInputField] = useState([
-        { itemName: '', price: '' }
-    ])
+    const [inputFields, setInputFields] = useState([
+        { itemName: ''}
+    ]);
+
+    const handleAddFields = (index) => {
+        setInputFields([...inputFields, { itemName: '' }])
+        console.log(index)
+    }
+
+    const handleRemoveFields = (index) => {
+        const values = [...inputFields];
+        values.splice(index, 1);
+        setInputFields(values);
+    }
+
+    const handleChangeInput = (index, event) => {
+        const values = [...inputFields];
+        console.log(values);
+        values[index].itemName = event.target.value
+        setInputFields(values);
+    }
 
 	return (
             <div class="min-h-screen flex items-center justify-center">
-
-            
-                <div class="bg-white p-8 rounded shadow-2xl w-1/2">
+                <div class="bg-white p-8 rounded shadow-2xl w-1/2">  
                     
-                    <h2 class="text-3xl font-bold mb-4">Menu Item</h2>
 
                     <form class="w-full max-w-lg">
                         { inputFields.map((inputField, index) => (
-                        <div>
-
-                                
-                            <div class="flex flex-wrap -mx-3 mb-6">
+                            
+                            <div class="flex flex-wrap -mx-3 mb-6" key={index}>
+                                <h2 class="text-3xl font-bold mb-4">Menu Item {index + 1}</h2>
                                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                                        First Name
+                                        Item Name
                                     </label>
-                                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane"/>
+                                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" value={inputField.itemName} id="grid-first-name" type="text" onChange={event => handleChangeInput(index, event)} />
                                     <p class="text-red-500 text-xs italic">Please fill out this field.</p>
                                 </div>
-                            </div>
-                            <PlusIcon className='text-indigo-500 : text-gray-400 group-hover:text-gray-500, -ml-0.5 mr-2 h-5 w-5'/>
-                        </div>
-                            
-
-                            
-               
-                             
+                                <PlusIcon onClick={(event) => handleAddFields(index+1)} className='text-indigo-500 : text-gray-400 group-hover:text-gray-500, -ml-0.5 mr-2 h-5 w-5'/>
+                                <MinusIcon onClick={(event) => handleRemoveFields(index)} className='text-indigo-500 : text-gray-400 group-hover:text-gray-500, -ml-0.5 mr-2 h-5 w-5'/>
+                            </div>     
                         ))}
                     </form>
                 </div>
